076e968434764fb33ec4fcda73222cf281488ea1
DudaElias <42540760+DudaElias@users.noreply.github.com>
2019-03-14 13:27:15 -0300

    Criação da classe Célula, com getters e setters

---
+++ MatrizEsparsa/MatrizEsparsa/Celula.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizEsparsa
+{
+    class Celula
+    {
+        protected Celula celulaDireita, celulaAbaixo;
+
+        protected int linha, coluna;
+
+        protected double valor;
+        public int Linha { get => linha; set =>  linha = value; }
+        public int Coluna { get => coluna; set => coluna = value; }
+        public double Valor { get => valor; set => valor = value; }
+        public Celula CelulaDireita { get => celulaDireita; set => celulaDireita = value; }
+        public Celula CelulaAbaixo { get => celulaAbaixo; set => celulaAbaixo = value; }
+
+
+    }
+}
