cc11e57661b04782c7638a02948a20b6120636d2
Scherer <fsvicentin@hotmail.com>
2019-03-16 15:18:02 -0300

    Fim da Celula e comeco da ListaCruzada

--- MatrizEsparsa/MatrizEsparsa/Celula.cs
+++ MatrizEsparsa/MatrizEsparsa/Celula.cs
@@ -1,18 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 /// <summary>
 /// Classe Celula que será usada para preencher a Lista duplamente ligada
 /// </summary>
 class Celula
 {
     //declaração de atributos da classe
-    protected Celula celulaDireita, celulaAbaixo; //ponteiros para as células que estão à direita e em baixo da célula this
+    protected Celula direita, abaixo; //ponteiros para as células que estão à direita e em baixo da célula this
     protected int linha, coluna; //valores que representam a posição da célula na matriz
     protected double valor; //valor que a célula tem
+    public static int posicaoDefault = 0;
 
     //propriedades
     /// <summary>
@@ -42,66 +38,110 @@ class Celula
     /// <summary>
     /// Propriedade do valor da célula com get e set
     /// </summary>
-    public double Valor { get => valor; set => valor = value; }
+    public double Valor
+    {
+        get => valor;
+        set
+        {
+            if (value != 0)
+                valor = value;
+        }
+    }
     /// <summary>
     /// Propriedade da célula à direita desta com get e set
     /// </summary>
-    public Celula Direita { get => celulaDireita; set => celulaDireita = value; }
+    public Celula Direita
+    {
+        get => direita;
+        set => direita = value;
+    }
     /// <summary>
     /// Propriedade da célula abaixo desta com get e set
     /// </summary>
-    public Celula Abaixo { get => celulaAbaixo; set => celulaAbaixo = value; }
+    public Celula Abaixo
+    {
+        get => abaixo;
+        set => abaixo = value;
+    }
 
     //construtores
     /// <summary>
     /// Construtor com todos os atributos
     /// </summary>
-    /// <param name="v">Valor que a célula terá</param>
+    /// <param name="v">Valor que a célula terá. Deve ser diferente de 0</param>
     /// <param name="l">Linha que a célula ocupará na matriz (começa em 1)</param>
     /// <param name="c">Coluna que a célula ocupará na matriz (começa em 1)</param>
     /// <param name="a">Objeto da célula abaixo desta na lista ligada</param>
     /// <param name="d">Objeto da célula à direita desta na lista ligada</param>
     public Celula(double v, int l, int c, Celula a, Celula d)
     {
+        if (v == 0)
+            throw new ArgumentOutOfRangeException("A célula não pode valer 0");
         Valor = v;
         Linha = l;
         Coluna = c;
         Abaixo = a;
         Direita = d;
     }
+
+    /// <summary>
+    /// Construtor com todos os atributos menos o valor. Feito para um nó-cabeça
+    /// </summary>
+    /// <param name="l">Linha que a célula ocupará na matriz (começa em 1)</param>
+    /// <param name="c">Coluna que a célula ocupará na matriz (começa em 1)</param>
+    /// <param name="a">Objeto da célula abaixo desta na lista ligada</param>
+    /// <param name="d">Objeto da célula à direita desta na lista ligada</param>
+    public Celula(int l, int c, Celula a, Celula d)
+    {
+        Linha = l;
+        Coluna = c;
+        Abaixo = a;
+        Direita = d;
+    }
+
     /// <summary>
     /// Construtor com o valor e as células adjacentes apenas
     /// </summary>
-    /// <param name="v">Valor que a célula terá</param>
+    /// <param name="v">Valor que a célula terá. Deve ser diferente de 0</param>
     /// <param name="a">Objeto da célula abaixo desta na lista ligada</param>
     /// <param name="d">Objeto da célula à direita desta na lista ligada</param>
     public Celula(double v, Celula a, Celula d)
     {
+        if (v == 0)
+            throw new ArgumentOutOfRangeException("A célula não pode valer 0");
+        Valor = v;
         Direita = d;
         Abaixo = a;
-        Linha = Coluna = -1; //a posição fica com o valor default (-1)
+        Linha = Coluna = posicaoDefault; //a posição fica com o valor default (-1)
     }
+
     /// <summary>
     /// Construtor com a posição apenas
     /// </summary>
-    /// <param name="v">Valor que a célula terá</param>
+    /// <param name="v">Valor que a célula terá. Deve ser diferente de 0</param>
     /// <param name="l">Linha que a célula ocupará na matriz (começa em 1)</param>
     /// <param name="c">Coluna que a célula ocupará na matriz (começa em 1)</param>
     public Celula(double v, int l, int c)
     {
+        if (v == 0)
+            throw new ArgumentOutOfRangeException("A célula não pode valer 0");
+        Valor = v;
         Linha = l;
         Coluna = c;
         Direita = Abaixo = null; //as células adjacentes ficam com o valor default (null)
     }
+
     /// <summary>
     /// Construtor com apenas o valor da célula
     /// </summary>
-    /// <param name="v">Valor que a célula terá</param>
+    /// <param name="v">Valor que a célula terá. Deve ser diferente de 0</param>
     public Celula(double v)
     {
+        if (v == 0)
+            throw new ArgumentOutOfRangeException("A célula não pode valer 0");
         Valor = v;
         //os outros atributos ficam com o valor default
-        Linha = Coluna = -1;
+        Linha = Coluna = posicaoDefault;
         Direita = Abaixo = null;
     }
 
@@ -110,7 +150,14 @@ class Celula
     {
         return $"[{Linha}, {Coluna}] - {Valor}";
     }
-                                                     //Duda, verifica esses dois pra mim pq eu n sei se ta certo
+    public int CompareTo(Celula c)
+    {
+        int diffLinhas = Linha - c.Linha;
+        if (diffLinhas == 0)
+            return Coluna - c.Coluna;
+        return diffLinhas;
+    }
+                                                        //Duda, verifica esses dois pra mim pq eu n sei se ta certo
     public override bool Equals(object obj)
     {
         if (obj == this)
@@ -134,5 +181,5 @@ class Celula
         ret = ret * 5 + valor.GetHashCode();
         return ret;
     }
-                                                    //sera que precisa considerar as Celular Diretia e Abaixo???
+                                                       //sera que precisa considerar as Celular Diretia e Abaixo???
 }
---
+++ MatrizEsparsa/MatrizEsparsa/ListaCruzada.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ListaCruzada
+{
+    Celula primeira, atual, anterior, acima;
+    int linhas, colunas;
+
+    //métodos obrigatórios
+    public override string ToString()
+    {
+        atual = primeira.Abaixo.Direita;
+        string ret = "{ ";
+        while(atual.Abaixo.Linha > primeira.Abaixo.Linha)
+        {
+            while(atual.Direita.Coluna > primeira.Direita.Coluna)
+            {
+                ret += atual.Valor + " ";
+                atual = atual.Direita;
+            }
+            ret += "\n";
+            atual = atual.Abaixo.Direita;
+        }
+        return ret + " }";
+    }
+    public override bool Equals(object obj)
+    {
+        if (obj == this)
+            return true;
+        if (obj == null)
+            return false;
+        ListaCruzada l = obj as ListaCruzada;
+        if (l.linhas != linhas)
+            return false;
+        if (l.colunas != colunas)
+            return false;
+        var atualThis = primeira.Abaixo.Direita;
+        var atualObj = l.primeira.Abaixo.Direita;
+        while (atualThis.Abaixo.Linha > primeira.Abaixo.Linha && atualObj.Abaixo.Linha > l.primeira.Abaixo.Linha)
+        {
+            while (atualThis.Direita.Coluna > primeira.Direita.Coluna && atualObj.Direita.Coluna > l.primeira.Direita.Coluna)
+            {
+                if (!atualThis.Equals(atualObj))
+                    return false;
+                atualThis = atualThis.Direita;
+                atualObj = atualObj.Direita;
+            }
+            atualThis = atualThis.Abaixo.Direita;
+            atualObj = atualObj.Abaixo.Direita;
+        }
+        return true;
+    }
+    public override int GetHashCode()
+    {
+        atual = primeira.Abaixo.Direita;
+        int ret = 34;
+        ret = ret * 2 + linhas.GetHashCode();
+        ret = ret * 2 + linhas.GetHashCode();
+        while (atual.Abaixo.Linha > primeira.Abaixo.Linha)
+        {
+            while (atual.Direita.Coluna > primeira.Direita.Coluna)
+            {
+                ret += ret * 3 + atual.GetHashCode();
+                atual = atual.Direita;
+            }
+            atual = atual.Abaixo.Direita;
+        }
+        return ret;
+    }
+    public object Clone()
+    {
+        ListaCruzada l = null;
+        try
+        {
+            l = new ListaCruzada(this);
+        }
+        catch (Exception e) { }
+        return l;
+    }
+
+    //construtores
+    public ListaCruzada(ListaCruzada l)
+    {
+        if (l == null)
+            throw new ArgumentNullException();
+        this.primeira = l.primeira;
+        this.atual = l.atual;
+        this.anterior = l.anterior;
+        this.acima = l.acima;
+        this.linhas = l.linhas;
+        this.colunas = l.colunas;
+    }
+    public ListaCruzada(int l, int c)
+    {
+        if (l < 1 || c < 1)
+            throw new ArgumentOutOfRangeException();
+        primeira = atual = anterior = acima =  null;
+        primeira = new Celula(Celula.posicaoDefault, Celula.posicaoDefault, null, null);
+        linhas = l;
+        colunas = c;
+        atual = primeira;
+        //Celula.posicaoDefault = 3;
+        for (int i = 0; i < c; i++)
+        {
+            atual.Direita = new Celula(Celula.posicaoDefault, i, null, null);
+            atual = atual.Direita;
+        }
+        atual = primeira;
+        for (int i = 0; i < l; i++)
+        {
+            atual.Abaixo = new Celula(i, Celula.posicaoDefault, null, null);
+            atual = atual.Abaixo;
+        }
+    }
+
+    public int Contar()
+    {
+        atual = primeira.Abaixo.Direita;
+        int ret = 0;
+        while (atual.Abaixo.Linha > primeira.Abaixo.Linha)
+        {
+            while (atual.Direita.Coluna > primeira.Direita.Coluna)
+            {
+                ret++;
+                atual = atual.Direita;
+            }
+            atual = atual.Abaixo.Direita;
+        }
+        return ret;
+    }
+    public bool EstaVazia
+    {
+        get => primeira == null;
+    }
+    public bool ExisteCelula(int l, int c)
+    {
+        bool achou = false;
+        bool fim = false;
+        atual = primeira.Abaixo.Direita;
+        while (atual.Abaixo.Linha > primeira.Abaixo.Linha && !fim)
+        {
+            while (atual.Direita.Coluna > primeira.Direita.Coluna && !fim)
+            {
+                if (atual.Coluna == c && atual.Linha < l)
+                {
+                    acima = atual;
+                }
+                else if (atual.Linha == l && atual.Coluna == c)
+                {
+                    fim = true;
+                    achou = true;
+                }
+                else if (atual.Linha > l && atual.Coluna > c)
+                {
+                    fim = true;
+                }
+                else
+                    atual = atual.Direita;
+            }
+            if (!fim)
+            {
+                anterior = atual;
+                atual = atual.Abaixo.Direita;
+            }
+        }
+        return achou;
+    }
+    public void Adicionar(Celula c)
+    {
+        if (c == null)
+            throw new ArgumentNullException();
+        if (EstaVazia)
+        {
+            primeira.Abaixo.Direita = c;
+            primeira.Direita.Abaixo = c;
+            c.Direita = primeira.Abaixo;
+            c.Abaixo = primeira.Direita;
+        }
+        else if (!ExisteCelula(c.Linha, c.Coluna))
+        {
+            c.Direita = atual;
+            anterior.Direita = c;
+            c.Abaixo = acima.Abaixo;
+            acima.Abaixo = c;
+        }
+    }
+}
