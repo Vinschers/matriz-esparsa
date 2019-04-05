3564f9ecaf0fdfa6f9cbe260fb956490afc4b18d
DudaElias <42540760+DudaElias@users.noreply.github.com>
2019-04-05 16:40:45 -0300

    qweqqwwqe

--- MatrizEsparsa/MatrizEsparsa/ListaCruzada.cs
+++ MatrizEsparsa/MatrizEsparsa/ListaCruzada.cs
@@ -397,52 +397,7 @@ class ListaCruzada
         Celula atual = primeira.Direita;
         while (atual.Coluna != c) { atual = atual.Direita; }
         atual = atual.Abaixo;
-        /*while (atual.Linha != Celula.posicaoDefault)
-        {
-            /*if (atual.Linha != atual.Abaixo.Linha - 1)
-                atual.Valor += k;
-            if (/*atual.Abaixo.Linha == Celula.posicaoDefault || atual.Linha != atual.Abaixo.Linha - 1)
-            {
-                atual.Valor += k;
-                atual = atual.Abaixo;
-            }
-            if(atual.Linha == atual.Abaixo.Linha - 1 && atual.Linha != Celula.posicaoDefault)
-            {
-                Celula anterior = primeira.Abaixo;
-                Celula acima = primeira.Direita;
-                while (acima.Direita.Coluna != atual.Coluna) acima = acima.Direita;
-                while (acima.Abaixo.Linha != atual.Linha) acima = acima.Abaixo;
-                while (anterior.Linha != atual.Linha)
-                { anterior = anterior.Abaixo; }
-                while (anterior.Direita.Coluna != atual.Coluna)
-                { anterior = anterior.Direita; }
-                Celula ce = new Celula(k, anterior.Linha, c, acima, anterior);
-                /*ce.Direita = anterior.Direita;
-                anterior.Direita = ce;
-                ce.Abaixo = acima.Abaixo;
-                acima.Abaixo = ce;
-                atual = ce;
-            }
-            else if (atual.Linha == atual.Abaixo.Linha - 1)
-            {
-                while (atual.Abaixo.Linha != Celula.posicaoDefault) atual = atual.Abaixo;
-                while (atual.Linha < linhas)
-                {
-                    Celula anterior = primeira.Abaixo;
-                    while (anterior.Linha != atual.Linha)
-                    { anterior = anterior.Abaixo; }
-                    while (anterior.Direita.Coluna != atual.Coluna)
-                    { anterior = anterior.Direita; }
-                    Celula ce = new Celula(k, anterior.Linha + 1, atual.Coluna, atual.Abaixo, anterior);
-                    ce.Direita = anterior.Direita;
-                    anterior.Direita = ce;
-                    ce.Abaixo = atual.Abaixo;
-                    atual.Abaixo = ce;
-                    atual = atual.Abaixo;
-                }
-                return;
-            }
-        }*/
+        int qtdLinhas = 0;
         while (atual.Linha != Celula.posicaoDefault)
         {
             Celula ce = null;
@@ -456,16 +411,19 @@ class ListaCruzada
                 acima.Abaixo = ce;
                 Celula anterior = primeira.Abaixo;
                 while (anterior.Linha != atual.Linha + 1) anterior = anterior.Abaixo;
-                //anterior = anterior.Direita;
                 if (anterior.Coluna != Celula.posicaoDefault)
                     while (anterior.Coluna != atual.Coluna) anterior = anterior.Direita;
                 ce.Direita = anterior.Direita;
                 anterior.Direita = ce;
-                if (ce.Linha == linhas)
+
+                if (qtdLinhas == linhas)
                     return;
             }
+            if (atual.Abaixo.Linha == atual.Linha + 1) { qtdLinhas++; }
             atual.Valor += k;
             atual = atual.Abaixo;
+            /*else */if (qtdLinhas == linhas)
+                return;
         }
         if (atual.Abaixo == atual)
         {
